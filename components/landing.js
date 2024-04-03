d0d85c00453e402e84b2e9a454e48a906d579b07
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-04-03 21:21:21 +0800

    Landing Page update

--- components/landing.js
+++ components/landing.js
@@ -6,6 +6,7 @@ const Landing = () => {
     <div className='background'>
       <div className='content'>
         <h1 className='title'>Be a PAHINUNGOD</h1>
+        <p className='subtitle'>Join the movement and re-imagine the world through volunteerism and radical change.</p>
         <div>
           <button className='volunteer-buttons'>Be a Volunteer</button>
           <button className='volunteer-buttons'>Be a Sponsor/Partner</button>
